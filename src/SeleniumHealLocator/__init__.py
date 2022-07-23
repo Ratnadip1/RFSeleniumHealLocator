78c5dcf917d73f7dbbab4dab8020f3ee85b7458e
Ratnadip Chaudhuri <ratnadip.chaudhuri@gmail.com>
2022-07-23 15:50:35 +0530

    Commit - TestSeleniumHealLibrary.py - Modification to include the error class - SeleniumHealLocator\__init.py -> Modification to include only keyword libraries.

--- src/SeleniumHealLocator/__init__.py
+++ src/SeleniumHealLocator/__init__.py
@@ -1,21 +1,23 @@
 from robotlibcore import DynamicCore
 from .keywords.ManageInformation import ManageInformation
 from .keywords.HealElement import HealElement
-from .utilities.ElementInformation import ElementInformation
+
+"""
+SeleniumHealLocator facilitates the Robot Framework by introducing the Keyword libraries.
+Listing the libraries containing keywords and setting the Dynamic Core libraries field with the keyword module
+classes exposes the keywords to Robot Framework and hence is used seamlessly.
+"""
 
 
 class SeleniumHealLocator(DynamicCore):
+    info_type = None
 
     def __init__(self):
-        # self.element_info = None
+        # Listing the libraries with Keywords to be used by Robot Framework
         libraries = [
             ManageInformation(),
-            ElementInformation(),
             HealElement()
         ]
+
+        # Invoking the DynamicCore superclass constructor with listed keyword library modules.
         DynamicCore.__init__(self, libraries)
-        for keyword in self.get_keyword_names():
-            print("Keyword: " + keyword)
-
-        pass
-
--- test/TestSeleniumHealLibrary.py
+++ test/TestSeleniumHealLibrary.py
@@ -1,8 +1,9 @@
 from src.SeleniumHealLocator import SeleniumHealLocator
+from robot.libraries.BuiltIn import RobotNotRunningError
 
 
 def test_Selenium_Library():
     try:
         SeleniumHealLocator()
     except Exception as ex:
-        assert 'RobotNotRunningError', ex.__cause__
+        assert RobotNotRunningError, ex
