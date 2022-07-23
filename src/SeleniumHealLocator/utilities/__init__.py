9f6f52b26a1f945bcba35051483c57159adc79f8
Ratnadip Chaudhuri <ratnadip.chaudhuri@gmail.com>
2022-07-23 12:58:27 +0530

    Commit - utilities\__init__.py - Switcher implementation for XML and Json Based Implementation of ElementInformation.py

--- src/SeleniumHealLocator/utilities/__init__.py
+++ src/SeleniumHealLocator/utilities/__init__.py
@@ -1,2 +1,15 @@
 from . import ElementInformation
 # from ..keywords.ManageInformation import ManageInformation
+from ..utilities.XmlFileOperator import XmlFileOperator
+from ..utilities.JsonFileOperator import JsonFileOperator
+
+
+class FileOperatorSwitcher:
+
+    @staticmethod
+    def get_File_Operator(file_type):
+        switcher = {
+            '.xml': XmlFileOperator(),
+            '.json': JsonFileOperator()
+        }
+        return switcher.get(file_type, None)
