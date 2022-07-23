8e774253154ade126afbba11aeb58b071a28af1f
Ratnadip Chaudhuri <ratnadip.chaudhuri@gmail.com>
2022-07-23 20:56:00 +0530

    Commit - XmlFileOperator.py - Modified threading lock and correction in call to super class constructor and methods.

--- src/SeleniumHealLocator/utilities/XmlFileOperator.py
+++ src/SeleniumHealLocator/utilities/XmlFileOperator.py
@@ -1,4 +1,5 @@
 import os
+import threading
 from xml.dom import minidom
 
 from ..utilities.ElementInformation import ElementInformation
@@ -13,8 +14,11 @@ Processing and retrieving information from Element Information file is also impl
 
 class XmlFileOperator(ElementInformation):
 
+    _lock = None
+
     def __init__(self):
-        super.__init__(self)
+        super()
+        XmlFileOperator._lock = threading.Lock()
 
     # Construct the file name as provided by the argument in
     # 'Initiate Healing Process' keywords in Robot Framework
@@ -32,20 +36,20 @@ class XmlFileOperator(ElementInformation):
     # Generates the XML file for the first time if it is not already available.
     # It is a one-time activity and creates all the required
     def generate_Element_Information(self):
-        super(self).generate_Element_Information()
+        super().generate_Element_Information()
         try:
-            with self._lock():
+            with XmlFileOperator._lock:
                 root = minidom.Document()
 
-            xml = root.createElement('elements')
-            root.appendChild(xml)
+                xml = root.createElement('elements')
+                root.appendChild(xml)
 
-            xml_str = root.toprettyxml(indent="\t")
+                xml_str = root.toprettyxml(indent="\t")
 
-            save_path_file = self.folder_location + os.sep + self.file_name
+                save_path_file = self.folder_location + os.sep + self.file_name
 
-            with open(save_path_file, "w") as f:
-                f.write(xml_str)
+                with open(save_path_file, "w") as f:
+                    f.write(xml_str)
         except HealLocatorException as exception:
             print(exception.with_traceback())
             pass
