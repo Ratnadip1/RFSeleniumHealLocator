cebdbea4a7ac19fff0411ff69f1668782fcc0233
Ratnadip Chaudhuri <ratnadip.chaudhuri@gmail.com>
2022-07-28 21:20:52 +0530

    Commit - HealElement.py - Retrieve current browser session from Robot Framework by loading SeleniumLibrary.

--- src/SeleniumHealLocator/store/PageStore.py
+++ src/SeleniumHealLocator/store/PageStore.py
@@ -2,10 +2,14 @@ import threading
 
 from .PageInfo import PageInfo
 from .. import ManageInformation
+from ..utilities.HealLog import HealLog
 
 """
 PageStore class stores the information of all the pages explored during the test run and 
-saves them in the Element Information file.
+saves them in the Element Information file. On initiation, it checks the available listed page nodes
+in Element Information file. On invoking the keyword *** Get Heal Locator ***, it checks if information
+of the current page for the targeted locator is available in PageStore. If the same is not available, the 
+current page information is constructed and added to the PageStore list.
 """
 
 
@@ -14,13 +18,21 @@ class PageStore(object):
     __lock = threading.Lock()
     __pageStoreInstance = None
 
+    # Constructor for PageStore object. Setup of the Page Store process.
+    # Initiates the page store dictionary with existing Element Information file data.
+    # Initiates the index based on existing data in ElementInfo value.
     def __init__(self):
         self.pageStoreDict = {}
         self.elementInfo = ManageInformation.get_Instance().get_Element_Info()
         self.populate_page_Store_Dict()
         self.page_id = len(self.pageStoreDict) + 1
+        self.logger = HealLog()
         pass
 
+    # Method:       get_Instance
+    # Type:         Class
+    # Description:  Singleton implementation of PageStore class
+    # Returns:      Singleton instance
     @classmethod
     def get_Instance(cls):
         if not cls.__pageStoreInstance:
@@ -31,11 +43,12 @@ class PageStore(object):
         return cls.__pageStoreInstance
 
     def get_Page_Info(self, url):
-        # Remove http_scope from baseUrl
+        # Initializing local variables.
         base_url = None
         path = None
         page_scope = None
 
+        # Remove http_scope from baseUrl
         if url.startswith("http"):
             page_scope = url[0: url.index("://") + 3]
             url = url[url.index("://") + 3:]
@@ -48,21 +61,25 @@ class PageStore(object):
             base_url = url[0:url.index("/")]
             path = url[url.index("/"):]
 
+        # Validates if the page is already listed in the Element Information file.
+        # If the PageInfo is already available in the Element Information file,
+        # it would return the corresponding page id of the PageInfo instance listed in dictionary.
         page_id = self.is_page_listed(base_url, path, page_scope)
 
+        # If the Page information for the current browser page is not listed in the PageStore
+        # dictionary, a PageInfo object with current Page information will be created and stored.
         if page_id is None:
             page = PageInfo(page_id=self.page_id, page_base_url=base_url, page_path=path, page_scope=page_scope)
-            print('Page information is not available in Page Store: ' + str(page))
+            self.logger.info('Page information is not available in Page Store: ' + str(page))
             self.pageStoreDict[str(self.page_id)] = page
             page_id = self.page_id
             self.page_id += 1
-            pass
         else:
-            print('Page information is already available in Page Store: '
-                  + str(base_url) + ' ' + str(path) + ' ' + str(page_scope))
+            self.logger.info('Page information is already available in Page Store: ' +
+                             str(base_url) + ' ' + str(path) + ' ' + str(page_scope))
         return page_id
-        pass
 
+    # Static method for formatting the url to store the value in PageInfo object.
     @staticmethod
     def format_page_url(url):
         if url.find("?") != -1:
@@ -71,6 +88,9 @@ class PageStore(object):
             url = url[0: url.index("#")]
         return url
 
+    # Method: is_page_listed
+    # Compares the PageInfo and retrieves the Index in dictionary if the entry is available
+    # or returns null.
     def is_page_listed(self, base_url, path, page_scope):
         dict_items = self.pageStoreDict.items()
         filtered_dict = dict(filter(lambda e: e[1].get_Page_Base_Url() == base_url
@@ -82,8 +102,8 @@ class PageStore(object):
         else:
             return list(filtered_dict.keys())[0]
 
+    # Retrieves the list of PageInfo from Element Information file and stores it in PageInfo.
     def populate_page_Store_Dict(self):
-
         page_list = self.elementInfo.getPageList()
         for page in page_list:
             self.pageStoreDict[str(page.get_Page_Id())] = page
